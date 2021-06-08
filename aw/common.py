38967742a84b8e71a4c26f627a9db96edf587d0c
baicaisir <junhua.future@gmail.com>
2021-06-08 20:02:34 +0800

    增加mitmproxy修改响应脚本及相关方法

--- aw/common.py
+++ aw/common.py
@@ -11,7 +11,7 @@ def logcatIos(logname='iphone.log'):
     :return:进程号
     """
     # p = subprocess.Popen('idevice_id -l', shell=True)
-    p = subprocess.Popen('idevicesyslog > %s' %logname, shell=True, cwd=Model.logdirpath)
+    p = subprocess.Popen('idevicesyslog > %s' % logname, shell=True, cwd=Model.logdirpath)
     return p.pid
 
 # 日志抓取完成，杀掉进程
@@ -22,10 +22,11 @@ def logcatIosKill(pid):
     :return:
     """
     if platform.system() == 'Windows':
-        subprocess.call('taskkill /PID %s' %pid,  shell=True)
+        subprocess.call('taskkill /PID %s' % pid, shell=True)
     else:
-        subprocess.call('kill %s' %pid, shell=True)
+        subprocess.call('kill %s' % pid, shell=True)
     return True
+
 
 # 从log中匹配关键字
 def logKeywordExist(pattern, filepath):
@@ -49,7 +50,9 @@ def logKeywordExist(pattern, filepath):
     return wordlist
 
     # 从log中匹配关键字
-def logBlockKeywordExist(pattern, filepath, blocksize=1*1024*1024):
+
+
+def logBlockKeywordExist(pattern, filepath, blocksize=1 * 1024 * 1024):
     """
     ：function :按照数据块大小匹配关键字
     :param pattern: 正则匹配规则
@@ -69,6 +72,7 @@ def logBlockKeywordExist(pattern, filepath, blocksize=1*1024*1024):
     # print(f'\033[0;32;47m{wordlist}\033[0m')
     return wordlist
 
+
 def keywordLogZip(loglist, keyword, i, start=1):
     """
     :function:生成pytest的断言参数
@@ -79,8 +83,9 @@ def keywordLogZip(loglist, keyword, i, start=1):
     :return: 返回的是pytest需要的格式--[('kayak',  True),  ('civic',  True),  ('forest',  False)]
     """
     keywordlist = [(keyword + str(i)) for i in range(start, i)]
-    testlist = list(zip([loglist] * len(keywordlist),  keywordlist))
+    testlist = list(zip([loglist] * len(keywordlist), keywordlist))
     return testlist
+
 
 def keywordListLogZip(loglist, keywordlist):
     """
@@ -89,7 +94,7 @@ def keywordListLogZip(loglist, keywordlist):
     :param keywordlist: 检测的关键字列表
     :return: 返回的是pytest需要的格式--[('kayak',  True),  ('civic',  True),  ('forest',  False)]
     """
-    testlist = list(zip([loglist] * len(keywordlist),  keywordlist))
+    testlist = list(zip([loglist] * len(keywordlist), keywordlist))
     return testlist
 
 
@@ -103,9 +108,11 @@ def getClassProperty(classname, index) -> object:
 
     return md.getClasses(classname, index)
 
+
 def getValueById(name):
     text = md.findByAccessibilityId(name).get_attribute('value')
     return text
+
 
 def clickById(idname, index):
     """
@@ -115,6 +122,7 @@ def clickById(idname, index):
     """
     return md.clickByid(idname, index)
 
+
 def checkValue(value):
     """
     用来断言使用
@@ -122,31 +130,37 @@ def checkValue(value):
     :return:
     """
     assert value
+
+
 def clickByAccessibilityId(idname):
     """
     通过name点击控件
-    :param name:
+    :param idname:
     :return:
     """
     return md.findByAccessibilityId(idname).click()
 
-def clickElementsByAccessibilityId(idname,index=0):
+
+def clickElementsByAccessibilityId(idname, index=0):
     """
     通过name点击控件
-    :param name:
+    :param idname:
+    :param index:
     :return:
     """
     return md.findElementsByAccessibilityId(idname)[index].click()
 
+
 def clickByXpath(xpath):
     """
     通过xpath点击控件
-    :param name:
+    :param xpath:
     :return:
     """
     return md.findByXpath(xpath).click()
 
-def wait(t = 1):
+
+def wait(t=1):
     """
     执行等待
     :param t:默认是一秒钟
@@ -154,33 +168,38 @@ def wait(t = 1):
     """
     return md.wait(t)
 
-def goBack(n = 1):
+
+def goBack(n=1):
     """
     返回键
     :param n: 点击次数
     :return:
     """
     for i in range(n):
-        print('返回第%s次'%i)
+        print('返回第%s次' % i)
         md.goBack()
         md.wait(1)
 
-def clickByLocation(x,y):
+
+def clickByLocation(x, y):
     """
     通过页面坐标点击---ios
     :param x: x轴坐标
     :param y: y轴坐标
     :return:
     """
-    md.tapByLocation(x,y)
+    md.tapByLocation(x, y)
+
 
 def clickCenter():
     """点击屏幕中心位置"""
     md.clickCenter()
 
+
 def quit():
     """设备退出"""
     return md.quit()
+
 
 def checkIdIsExist(idname):
     """判断是否存在某个ID/name"""
@@ -188,3 +207,35 @@ def checkIdIsExist(idname):
         return True
     else:
         return False
+
+def changeFileContent(pattern, repl, filepath):
+    """
+    利用正则替换文件内容
+    :param pattern:正则规则
+    :param repl:替换的字符串
+    :param filepath:文件路径
+    :return:
+    """
+    with open(file=filepath, encoding='utf-8') as f:
+        buffer = f.read()
+        pa1 = re.compile(pattern).findall(buffer)[0]
+        filecontent = re.sub(pa1, repl, buffer)
+        print(filecontent)
+
+    with open(filepath, mode='w+', encoding='utf-8') as f:
+        f.write(filecontent)
+
+def setWinProxy(host='127.0.0.1', post=8888, clear=False):
+    """
+    win设置代理/清除代理
+    :param host: 代理ip
+    :param post: 代理端口
+    :param clear: 是否清除代理
+    :return:
+    """
+    if clear:
+        subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f && reg add "HKCU\Softwae \Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f', shell=True)
+        print('清除系统代理成功')
+    else:
+        subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f && reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "%s:%s" /f'% (host, post), shell=True)
+        print('设置代理成功')
---
+++ resource/adproxy.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/4 19:39
+# @Author  : 
+# @File    : adproxy.py
+# @Software: PyCharm
+
+import json
+import pymysql
+
+from mitmproxy import http
+
+ad_source, ad_type, system = "AdMod", 4, "ios"
+
+def getdata(ad_source, ad_type, system):
+    db = pymysql.connect("10.8.1.200", "qaplatform", "qaplatform", "qaplatform")
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("SELECT config from sdk_network_info where ad_source='%s' and ad_type=%d and system='%s'" % (ad_source, int(ad_type), system))
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
+    print("数据格式%s" % data[0])
+    # 关闭数据库连接
+    db.close()
+    return data[0]
+
+
+def response(flow: http.HTTPFlow):
+    # 加上过滤条件
+    print(flow.request.pretty_url, '=================================')
+    addata = getdata(ad_source, ad_type, system)
+    addata = json.loads(addata)
+
+    if "ssp-3-9f58.trnox.com/ssp/ws/sdk/mediation/ad/config/info/" in flow.request.pretty_url:
+        # 把响应数据转化成python对象，保存到data中
+        data = json.loads(flow.response.content)
+        print('全部数据为', data)
+        print('替换数据为', addata)
+        data['responseData']['noxmobiMediationConfig']['highRequestPool']['adRequestSettings'] = [addata]
+        flow.response.text = json.dumps(data)
+        print('相应数据为', flow.response.text)
